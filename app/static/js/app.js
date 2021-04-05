47894eeb130f0379a0d3f0a66e7f049056f5db74
Joanna-Joy Gray <jjgray21@hotmail.com>
2021-04-04 23:30:24 -0500

    Added routes

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,57 +1,6 @@
-/* Add your Application JavaScript */
-const app = Vue.createApp({
-  data() {
-    return {
-      welcome: 'Hello World! Welcome to VueJS'
-    }
-  }
-});
 
-app.component('app-header', {
-  name: 'AppHeader',
-  template: `
-      <header>
-          <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-            <a class="navbar-brand" href="#">VueJS App</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">News</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-      </header>    
-  `,
-  data: function() {
-    return {};
-  }
-});
-
-app.component('app-footer', {
-  name: 'AppFooter',
-  template: `
-      <footer>
-          <div class="container">
-              <p>Copyright &copy {{ year }} Flask Inc.</p>
-          </div>
-      </footer>
-  `,
-  data: function() {
-      return {
-          year: (new Date).getFullYear()
-      }
-  }
-})
-
-app.component('news-list', {
+// app.component('news-list', {
+const NewsList = {
   name: 'NewsList',
   template: `
     <div class="form-inline d-flex justify-content-center">
@@ -116,6 +65,91 @@ search term here" />
     }
   }
 
+//})
+};
+
+const Home = {
+  name: 'Home',
+  template: `
+    <div class="home">
+    <img src="/static/images/logo.png" alt="VueJS Logo">
+    <h1>{{ welcome }}</h1>
+    </div>
+  `,
+  data() {
+    return {
+      welcome: 'Hello World! Welcome to VueJS'
+    }
+  }
+};
+
+/* Add your Application JavaScript */
+const app = Vue.createApp({
+  data() {
+    return {
+      welcome: 'Hello World! Welcome to VueJS'
+    }
+  },
+  components: {
+    'home': Home,
+    'news-list': NewsList
+  }
+});
+
+app.component('app-header', {
+  name: 'AppHeader',
+  template: `
+      <header>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+            <a class="navbar-brand" href="#">VueJS App</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  
+                  <router-link to="/" class="nav-link">Home</router-link>
+                </li>
+                <li class="nav-item">
+                 
+                 <router-link to="/news" class="nav-link">News</router-link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+      </header>    
+  `,
+  data: function() {
+    return {};
+  }
+});
+
+app.component('app-footer', {
+  name: 'AppFooter',
+  template: `
+      <footer>
+          <div class="container">
+              <p>Copyright &copy {{ year }} Flask Inc.</p>
+          </div>
+      </footer>
+  `,
+  data: function() {
+      return {
+          year: (new Date).getFullYear()
+      }
+  }
 })
 
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/news', component: NewsList }
+  ]
+});
+
+app.use(router)
 app.mount('#app');
