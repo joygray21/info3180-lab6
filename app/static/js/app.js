460e20e3e1632cf256d7165aca1299073ace9fb1
Joanna-Joy Gray <jjgray21@hotmail.com>
2021-04-04 23:03:50 -0500

    Added search input field

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -54,6 +54,15 @@ app.component('app-footer', {
 app.component('news-list', {
   name: 'NewsList',
   template: `
+    <div class="form-inline d-flex justify-content-center">
+      <div class="form-group mx-sm-3 mb-2">
+        <label class="sr-only" for="search">Search</label>
+        <input type="search" name="search" v-model="searchTerm"
+id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter
+search term here" />
+        <p>You are searching for {{ searchTerm }}</p>
+      </div>
+    </div>
     <div class="news">
       <h2>News</h2>
       <ul class="news__list">
@@ -83,7 +92,8 @@ app.component('news-list', {
   },
   data() {
     return {
-      articles: []
+      articles: [],
+      searchTerm: ''
     }
   }
 
